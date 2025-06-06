4340debf7bda1ca2ccb99ce83ca83c12f558621b
Abbos1101 <104965551+Abbos1101@users.noreply.github.com>
2025-06-06 10:45:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -24,4 +24,4 @@ conn = mysql.connector.connect(
     host=os.getenv("MYSQL_HOST"),
     user=os.getenv("MYSQL_USER"),
     password=os.getenv("MYSQL_PASSWORD"),
-    database=os.getenv("MYSQL_DATABAS_
+    database=os.getenv("MYSQL_DATABASE")
